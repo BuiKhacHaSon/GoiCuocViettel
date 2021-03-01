7298ae7a9352e9738d2406b126ddce1407050dd0
bkh-son <buikhachason@gmail.com>
2021-03-01 11:01:22 +0700

    Add project files.

---
+++ Models/DetailView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoiCuoc.Models
+{
+    public class DetailView
+    {
+        public string Name { get; set; }
+        public string Test { get; set; }
+    }
+}
